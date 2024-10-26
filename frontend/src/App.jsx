2cec10629fc617bd6bb96593c010249bcdf0b6bb
Franklin Kuba <franklinkuba7@gmail.com>
2024-10-26 18:54:56 -0500

    app setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return <div className="text-3xl text-red-600">app</div>;
+  return <div className="text-3xl text-red-600">app test</div>;
 }
