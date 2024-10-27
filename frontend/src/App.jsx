814110d822ccf8827331b9feddc65a27afa73026
Franklin Kuba <franklinkuba7@gmail.com>
2024-10-27 00:57:13 -0500

    Header update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
 // components
 import Header from "./components/Header";
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,22 @@
-import { Navbar } from "flowbite-react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <Navbar className="border-b-2">
-      <Link to="/" className="self-center whitespace-nowrap text-sm">
+    <nav className="navbar">
+      <Link to="/" className="blog-name">
         Vexspire
       </Link>
-    </Navbar>
+      <div className="nav-links">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/about" className="nav-link">
+          About
+        </Link>
+        <Link to="/login" className="nav-link">
+          Login
+        </Link>
+      </div>
+    </nav>
   );
 }
