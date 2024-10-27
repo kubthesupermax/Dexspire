2fa25eba2f92910000fbcf050fd7ce7c6d69e9f0
Franklin Kuba <franklinkuba7@gmail.com>
2024-10-26 21:07:36 -0500

    Components setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// components
+import Header from "./components/Header";
+
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -11,6 +14,7 @@ import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <Navbar className="border-b-2">
+      <Link to="/" className="self-center whitespace-nowrap text-sm">
+        Vexspire
+      </Link>
+    </Navbar>
+  );
+}
